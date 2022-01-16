b45757501f79f8f05224b7d28a8c24a6cec31201
michael Means <colemeans51@gmail.com>
2022-01-16 14:34:52 -0600

    practice

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-
-var a = 'hello world';
-
 function b() {
-    console.log('called b');
+    var myVar;
+    console.log(myVar);
 }
 
-b();
-console.log(a);
+function a() {
+    var myVar = 2;
+    console.log(myVar);
+    b();
+}
+
+var myVar = 1;
+console.log(myVar);
+a();
+console.log(myVar);
