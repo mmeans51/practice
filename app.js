d597f7bf71f4a37b7a61924d22a704702e41ff45
michael Means <colemeans51@gmail.com>
2022-01-31 13:54:31 -0600

    d

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log(libraryName)
+var a = 3;
+var b;
+b = a;
+a = 2;
+
+console.log(a,b);
+
+// by reference
+var c = {
+  greeting: 'hi'
+};
+var d;
+
+d = c;
+c.greeting = 'hello';
+console.log(c);
+console.log(d);
+
+
+
+function changeGreeting(obj) {
+  obj.greeting = 'hola';
+}
+
+changeGreeting(d);
+console.log(c,d);
+
+c = { greeting: 'howdy' };
+
+console.log(c)
+console.log(d);
