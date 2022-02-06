69ea64f3799c5ba7915437f77ea3dd18a40c6b21
michael Means <colemeans51@gmail.com>
2022-02-06 11:36:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-function greet(whattosay) {
+function buildFunctions() {
 
-  return function(name) {
-    console.log(whattosay + ' ' + name);
-  }
+    var arr = [];
+
+    for (var i = 0; i < 3; i++) {
+      arr.push(
+        function() {
+          console.log(i);
+        }
+      )
+    }
+    return arr;;
+
 }
 
- var sayHi = greet('hi');
- sayHi('tony');
+var fs = buildFunctions();
+fs[0]();
+fs[1]();
+fs[2]();
