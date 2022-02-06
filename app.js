04f049c2b76fbfc7897e7d2b8e4e5570682a432d
michael Means <colemeans51@gmail.com>
2022-02-06 14:29:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,2 @@
-var person = {
-  firstname: 'john',
-  lastname: 'doe',
-  getFullName: function() {
-    var fullname = this.firstname + ' ' + this.lastname;
-    return fullname;
-  }
-}
-
-var logName = function(lang1, lang2) {
-  console.log('logged: ' + this.getFullName());
-  console.log('arguments: ' + lang1 + ' ' + lang2);
-}
-
-var logPersonName = logName.bind(person);
-
-logPersonName();
-
-logName.call(person, 'en', 'es');
-logName.apply(person, ['en', 'es'])
+var a = {};
+var b = function() 
